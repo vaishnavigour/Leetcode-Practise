072dfb2a980e3f78df53770ac42ccad8317000a5
vaishnavigour <85483450+vaishnavigour@users.noreply.github.com>
2022-06-17 16:29:31 +0530

    Time: 7 ms (98.85%), Space: 47.5 MB (81.00%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,29 +1,25 @@
 class Solution {
-     int compareInterval(int[] i1, int[] i2){
-        return i1[0]-i2[0];
-    }
     public int[][] merge(int[][] intervals) {
-        
-        if(intervals.length ==1 || intervals.length ==0)
+        if(intervals.length ==0 || intervals.length ==1)
             return intervals;
-        int n= intervals.length;
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        int res=0; 
-        for(int i=1; i<n; i++){
+        int res=0;
+        Arrays.sort(intervals , (a,b) -> Integer.compare(a[0], b[0]));
+        for(int i=1; i< intervals.length; i++){
             if(intervals[res][1] >= intervals[i][0]){
-                //merge
-                intervals[res][1] = Math.max(intervals[res][1], intervals[i][1]);
                 intervals[res][0] = Math.min(intervals[res][0], intervals[i][0]);
+                intervals[res][1] = Math.max(intervals[res][1], intervals[i][1]);
+
             }
             else{
                 res++;
                 intervals[res] = intervals[i];
             }
+           
         }
-        int[][] ans = new int[res+1][];
-        for(int i=0 ; i<=res ;i++){
-             ans[i] = intervals[i];
-        }        
+         int ans[][] = new int[res+1][];
+            for(int i=0; i<=res ; i++){
+                ans[i] = intervals[i];
+            }
         return ans;
     }
    
