01b7c3774a0f5e1107c5d2e2953493c672d6368e
vaishnavigour <85483450+vaishnavigour@users.noreply.github.com>
2022-07-05 18:11:24 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (58.41%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+         int prev2 =1;
+        int prev=1;
+        for(int i=2;i<=n;i++){
+            int curri = prev2+prev;
+            prev2 = prev;
+            prev = curri;
+        }
+        return prev;
+    }
+}
