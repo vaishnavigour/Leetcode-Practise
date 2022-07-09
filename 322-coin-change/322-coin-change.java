8346fbb2b83aae894e624664cea17a6ab4968153
vaishnavigour <85483450+vaishnavigour@users.noreply.github.com>
2022-07-09 19:36:40 +0530

    Time: 28 ms (54.78%), Space: 47.7 MB (23.57%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,25 +2,28 @@ class Solution {
     int MAX=(int)10000000;
    
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length][amount+1];
+        int[] prev = new int[amount+1];
+         int[] curr = new int[amount+1];
         for(int target = 0;target<=amount;target++){
             if(target%coins[0]==0) 
-                dp[0][target] = target/coins[0];
+                prev[target] = target/coins[0];
             else
-                dp[0][target] =MAX;
+                prev[target] =MAX;
         }
         for(int ind=1;ind<coins.length;ind++){
             for(int target =0 ; target <=amount ;target++){
-                int nottake= dp[ind-1][target];
+                int nottake= prev[target];
                 int take = MAX;
                 if(coins[ind]<=target)
-                    take = 1+dp[ind][target-coins[ind]];
-                dp[ind][target] =Math.min(take,nottake);
+                    take = 1+curr[target-coins[ind]];
+                curr[target] =Math.min(take,nottake);
             }
+            for(int i=0;i<=amount;i++)
+                prev[i] = curr[i];
         }
         
-        if( dp[coins.length-1][amount] >= MAX)
+        if( prev[amount] >= MAX)
             return -1;
-        return dp[coins.length-1][amount];
+        return prev[amount];
     }
 }
