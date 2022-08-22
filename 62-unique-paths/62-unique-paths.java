f42e85b8685293aac9c09dd60dd184fd67cf57ad
vaishnavigour <85483450+vaishnavigour@users.noreply.github.com>
2022-08-22 22:39:22 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (94.61%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,17 +1,42 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
-        for(int i=0; i<m; i++){
+    public int f(int i,int j, int[][] dp){
+        if(i==0 && j==0)
+            return 1;
+        if(i<0 || j<0)
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        return dp[i][j] = f(i-1 , j,dp)+f(i,j-1,dp);
+    }
+    public int uniquePaths(int m, int n){
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m; i++){
             Arrays.fill(dp[i] , -1);
         }
-        return recur(0, 0, m, n, dp);
+        return f(m-1,n-1,dp);
     }
-    public int recur(int row, int col, int m, int n, int[][] dp){
-    
-        if(row == m-1 && col ==n-1) return 1;
-        if(row >= m || col >= n) return 0;
-        if(dp[row][col] != -1)
-                return dp[row][col];
-        return dp[row][col] = recur(row+1, col, m, n, dp) +recur(row, col+1, m, n, dp );
-    }
+//     public int f(int i,int j , int[][] dp){
+//         if(i==0 && j==0){
+//                         System.out.println("hi");
+
+//             return 1;
+//         }
+//         if(i<1 || j<1)
+//             return 0;
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
+//         int up = f(i-1,j,dp);
+//         int left =f(i,j-1,dp);
+//         dp[i][j] = up+left;
+//         return dp[i][j];
+//     }
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         for(int i=0; i<m ; i++){
+//             Arrays.fill(dp[i],-1);
+//         }
+//         return f(m-1,n-1,dp);
+        
+//     }
 }
